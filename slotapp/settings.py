1470e4a27ce84a697e1060ee5153273f17a550ca
Tülin Izer <tulinizer@gmail.com>
2017-12-20 21:05:47 +0300

    Remove authentication classes

--- slotapp/settings.py
+++ slotapp/settings.py
@@ -42,9 +42,8 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAdminUser',
-    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [],
+    'DEFAULT_PERMISSION_CLASSES': [],
     'PAGE_SIZE': 10
 }
 
